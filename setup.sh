9307aec4741ef7e94521a14759c1fbab36ff326a
pavanpc <pcpavan.471@gmail.com>
2016-06-11 01:48:02 +0530

    enabled logging

--- setup.sh
+++ setup.sh
@@ -1,24 +1,23 @@
-#./dependencies/elasticsearch/elasticsearch-2.2.0/bin/elasticsearch -d
-#echo "Staring elasticsearch......."
-#sleep 10s
-#curl localhost:9200
-#curl -XPUT 'http://localhost:9200/events/events/session_start1465484047'  -d '{
-    #    "event_type" : "session_start",
-     #   "ts" : 1465484047,
-     #   "platform" : "Android",
-     #   "params" : {
-     #     "Location service status" : "Location service On"
-     #   },
-     #   "session_uuid" : "4c71b97e79fbbd5599945903e77065cef8a93cf8",
-     #   "id" : "session_start1465484047"
-     # }'
+./dependencies/elasticsearch/elasticsearch-2.2.0/bin/elasticsearch -d
+echo "Staring elasticsearch......."
+sleep 10s
+curl localhost:9200
+curl -XPUT 'http://localhost:9200/events/events/session_start1465484047'  -d '{
+        "event_type" : "session_start",
+        "ts" : 1465484047,
+        "params" : {
+          "Location service status" : "Location service On",
+          "os_type": "andrioid"
+        },
+        "id" : "session_start1465484047"
+      }'
 echo "Successfully started elasticsearch !!!!"
 
 echo "Staring kafka......."
-./dependencies/kafka_2.11-0.8.2.0/bin/zookeeper-server-start.sh dependencies/kafka_2.11-0.8.2.0/config/zookeeper.properties & > /dev/null
+./dependencies/kafka_2.11-0.8.2.0/bin/zookeeper-server-start.sh dependencies/kafka_2.11-0.8.2.0/config/zookeeper.properties  > application.log  2>&1
 sleep 5s
 echo "Started kafka zookeper...."
-./dependencies/kafka_2.11-0.8.2.0/bin/kafka-server-start.sh  dependencies/kafka_2.11-0.8.2.0/config/server.properties & > /dev/null
+./dependencies/kafka_2.11-0.8.2.0/bin/kafka-server-start.sh  dependencies/kafka_2.11-0.8.2.0/config/server.properties  >  application.log  2>&1
 sleep 5s
 echo "Started kafka server...."
 ./dependencies/kafka_2.11-0.8.2.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic get_social_events 
@@ -33,7 +32,8 @@ echo "Staring dropwizard service....."
 
 cd dependencies/dropwizard/
 
-java -jar getSocialEvents-1.0-SNAPSHOT.jar server configuration.yml & > /dev/null
+java -jar getSocialEvents-1.0-SNAPSHOT.jar server configuration.yml  >  application.log  2>&1
 
 cd ../..
 echo "Successfully started dropwizard service  !!!!"
+echo "Check application.log for  above services(elasticsearch, kafka) start up/logging details "
